a6c0a0c07f88bdd324c0ac20e53da60cf62a2009
RamonDev658 <ramondev658@gmail.com>
2023-03-12 12:00:15 -0300

    Refactor: added the software architecture.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const userRoute= require('./src/routes/user.route')
 const app = express()
 
-app.get('/',  (req, res) =>{
-  res.send('Hello World')
-})
+app.use("/soma", userRoute)
+//Rota
+    //http method(CRUD<CREATE,READ,UPDATE,DELETE>),
+        //GET- pega uma info
+        //POST- Cria uma info
+        //PUT-Altera toda a info
+        //PATCH-Altera parte da info
+        //DELETE-Apaga uma info
+    //Name>Um identificador da rota,
+    //Function(Callback)-responsável por executar algum comando.
 
 app.listen(3000)
