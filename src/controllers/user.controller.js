c18384e14ee3e70dfa274ae76f58d1cfca4909ab
RamonDev658 <ramondev658@gmail.com>
2023-03-12 20:40:48 -0300

    Refactor: locating users in MongoDB successfully.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,16 @@
-const userService = require('../services/user.service')
+const userService = require("../services/user.service");
+const mongoose = require("mongoose");
 
-const create = async (req,res) => {
- const {name, username, email, password, avatar,background} = req.body;
+const create = async (req, res) => {
+  const { name, username, email, password, avatar, background } = req.body;
 
-  if(!name || !username || !email || !password || !avatar || !background) {
-  res.status(400).send({message:"Submit all fields for registration"})
+  if (!name || !username || !email || !password || !avatar || !background) {
+    res.status(400).send({ message: "Submit all fields for registration" });
   }
-  const user = await userService.create(req.body);
+  const user = await userService.createService(req.body);
 
-  if(!user) {
-    return res.status(400).send({message:"Error creating user"});
+  if (!user) {
+    return res.status(400).send({ message: "Error creating user" });
   }
 
   res.status(201).send({
@@ -20,8 +21,30 @@ const create = async (req,res) => {
       username,
       email,
       avatar,
-      background
+      background,
     },
-  })
+  });
 };
-module.exports ={ create };
+
+const findAll = async (req, res) => {
+  const users = await userService.findAllService();
+  if (users.length === 0) {
+    return res.status(400).send({ message: "There are no users registered." });
+  }
+  res.send(users);
+};
+const findById = async (req, res) => {
+  const id = req.params.id;
+ if(!mongoose.Types.ObjectId.isValid(id)) {
+  return res.status(400).send({ message: "Invalid Id, try again!" });
+ }
+
+  const user =await userService.findByIdService(id);
+
+  if (!user) {
+    return res.status(400).send({ message: "User not found." });
+  }
+  res.send(user);
+};
+
+module.exports = { create, findAll, findById };
