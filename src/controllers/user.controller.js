119e79432970c982e102626328bb386576b50ff6
RamonDev658 <ramondev658@gmail.com>
2023-03-12 21:39:57 -0300

    Refactor: Updating a user using patch method.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -35,16 +35,43 @@ const findAll = async (req, res) => {
 };
 const findById = async (req, res) => {
   const id = req.params.id;
- if(!mongoose.Types.ObjectId.isValid(id)) {
-  return res.status(400).send({ message: "Invalid Id, try again!" });
- }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ message: "Invalid Id, try again!" });
+  }
 
-  const user =await userService.findByIdService(id);
+  const user = await userService.findByIdService(id);
 
   if (!user) {
     return res.status(400).send({ message: "User not found." });
   }
   res.send(user);
+};
+const update = async (req, res) => {
+  const { name, username, email, password, avatar, background } = req.body;
+
+  if (!name && !username && !email && !password && !avatar && !background) {
+    res.status(400).send({ message: "Submit at list one field for update" });
+  }
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ message: "Invalid Id, try again!" });
+  }
+  const user = await userService.findByIdService(id);
+
+  if (!user) {
+    return res.status(400).send({ message: "User not found." });
+  }
+  await userService.updateService(
+    id,
+    name,
+    username,
+    email,
+    password,
+    avatar,
+    background
+  );
+  res.send({ message: "User updated successfully." });
 };
 
 module.exports = { create, findAll, findById };
